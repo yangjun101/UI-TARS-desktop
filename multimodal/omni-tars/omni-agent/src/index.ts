3b28c9b27209d9250feb3921c037addb9990013a
ULIVZ <chenhaoli@bytedance.com>
2025-09-06 18:21:09 +0800

    chore(o-agent): update example prompts (#1417)

--- multimodal/omni-tars/omni-agent/src/index.ts
+++ multimodal/omni-tars/omni-agent/src/index.ts
@@ -42,7 +42,14 @@ export default class OmniTARSAgent extends ComposableAgent {
       'Tell me the top 5 most popular projects on ProductHunt today',
       'Write hello world using python',
       'Use jupyter to calculate who is greater in 9.11 and 9.9',
+      'Write code to reproduce seed-tars.com',
+      'Summary seed-tars.com/1.5',
       'Write a python code to download the paper https://arxiv.org/abs/2505.12370, and convert the pdf to markdown',
+      'Search news about bytedance seed1.6 model, then write a web page in modern style and deploy it',
+      'Write a minimal code sample to help me use transformer',
+      'Please search for trending datasets on Hugging Face, download the top-ranked dataset, and calculate the total number of characters in the entire datase.',
+      `Identify the independence process of a twin-island nation where the pro-self-governance political group won thirteen out of seventeen legislative seats in spring 1980 national polls, a second constitutional conference was held at a historic London venue in late 1980, liberation from colonial rule is annually commemorated on November 1st as a public holiday, and an agreement revised the smaller island's local governance legislation for enhanced autonomy. What was the composition of the associated state that preceded its independence?`,
+      `I am a high school music theory teacher and i'm preparing a course on basic music theory to explain knowledge about music names, roll titles, major scales, octave distribution, and physical frequency. Please help me collect enough informations,  design fulfilling and authoritative course content with demonstration animations,  and finally output them as web page`,
     ],
     workspace: {
       navItems: [
